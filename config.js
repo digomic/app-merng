8e735fbafa93f406e139696f958c53686068331e
unknown <digomic@outlook.es>
2021-01-15 06:09:14 -0600

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://admin:9ingQ3Vygj9TaLo2@cluster0.4atqm.mongodb.net/mern?retryWrites=true&w=majority',
+    SECRET_KEY: 'pepitopelapapasconunpicopelapapas'
+}
